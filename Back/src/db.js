129389251c141597ae65e3d9efff2386b5a3e2fd
Ubuntu <ubuntu@ip-172-31-42-197.us-east-2.compute.internal>
2026-06-06 00:32:06 +0000

    Corrige variaveis de ambiente do PostgreSQL

--- Back/src/db.js
+++ Back/src/db.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
+  user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
 
